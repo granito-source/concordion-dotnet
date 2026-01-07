cc6e823547fdb37c7bda5cccf1886c3ae76fbbf8
Alexei Yashkov <alexei.yashkov@yashkov.org>
2026-01-07 16:02:16 -0500

    Redo operator tests

--- OGNL.Test/ArithmeticAndLogicalOperatorsTest.cs
+++ OGNL.Test/ArithmeticAndLogicalOperatorsTest.cs
@@ -1,5 +1,6 @@
 //--------------------------------------------------------------------------
 //  Copyright (c) 2004, Drew Davidson and Luke Blanshard
+//  Copyright (c) 2026, Alexei Yashkov
 //  All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
@@ -29,73 +30,82 @@
 //  DAMAGE.
 //--------------------------------------------------------------------------
 
-using OGNL.Test.Util;
-
 namespace OGNL.Test;
 
-public class ArithmeticAndLogicalOperatorsTest : OgnlTestCase {
+[TestFixture]
+public class ArithmeticAndLogicalOperatorsTest : OgnlFixture {
     private static readonly object[][] Tests = [
-        // Double-valued arithmetic expressions
-        ["-1d", (double)-1],
-        ["+1d", (double)1],
+        // Floating point arithmetic expressions
+        ["-1d", -1d],
+        ["+1d", 1d],
         ["--1f", 1f],
-        ["2*2.0", (double)4],
-        ["5/2.", 2.5],
-        ["5+2D", (double)7],
-        ["5f-2F", (float)3],
-        ["5.+2*3", (double)11],
-        ["(5.+2)*3", (double)21],
+        ["2 * 2.0", 4d],
+        ["5 / 2.", 2.5],
+        ["5 + 2D", 7d],
+        ["5f - 2F", 3f],
+        ["5. + 2 * 3", 11d],
+        ["(5. + 2) * 3", 21d],
 
         // BigDecimal-valued arithmetic expressions
         ["-1b", (decimal)-1],
         ["+1b", (decimal)1],
         ["--1b", (decimal)1],
-        ["2*2.0b", (decimal)4.0],
-        ["5/2.B", (decimal)2.5],
-        ["5.0B/2", (decimal)2.5],
-        ["5+2b", (decimal)7],
-        ["5-2B", (decimal)3],
-        ["5.+2b*3", (decimal)11],
-        ["(5.+2b)*3", (decimal)21],
+        ["2 * 2.0b", (decimal)4.0],
+        ["5 / 2.B", (decimal)2.5],
+        ["5.0B / 2", (decimal)2.5],
+        ["5 + 2b", (decimal)7],
+        ["5 - 2B", (decimal)3],
+        ["5. + 2b * 3", (decimal)11],
+        ["(5. + 2b) * 3", (decimal)21],
 
         // Integer-valued arithmetic expressions
         ["-1", -1],
         ["+1", 1],
         ["--1", 1],
-        ["2*2", 4],
-        ["5/2", 2],
-        ["5+2", 7],
-        ["5-2", 3],
-        ["5+2*3", 11],
-        ["(5+2)*3", 21],
+        ["2 * 2", 4],
+        ["5 / 2", 2],
+        ["5 + 2", 7],
+        ["5 - 2", 3],
+        ["5 + 2 * 3", 11],
+        ["(5 + 2) * 3", 21],
         ["~1", ~1],
-        ["5%2", 1],
-        ["5<<2", 20],
-        ["5>>2", 1],
-        ["5>>1+1", 1],
-        ["-5>>2", -5 >> 2],
-        ["-5L>>2", -5L >> 2],
+        ["5 % 2", 1],
+        ["5 << 2", 20],
+        ["5 >> 2", 1],
+        ["5 >> 1 + 1", 1],
+        ["-5 >> 2", -5 >> 2],
+        ["-5L >> 2", -5L >> 2],
         ["5. & 3", (double)1],
-        ["5 ^3", 6],
-        ["5l&3|5^3", 7L],
-        ["5&(3|5^3)", 5],
+        ["5 ^ 3", 6],
+        ["5l & 3 | 5 ^ 3", 7L],
+        ["5 & (3 | 5 ^ 3)", 5],
 
         // Logical expressions
         ["!1", false],
         ["!null", true],
-        ["5<2", false],
-        ["5>2", true],
-        ["5<=5", true],
-        ["5>=3", true],
+        ["5 < 2", false],
+        ["5 > 2", true],
+        ["5 <= 5", true],
+        ["5 >= 3", true],
+        ["\"one\" > \"two\"", false],
+        ["\"one\" >= \"two\"", false],
+        ["\"one\" < \"two\"", true],
+        ["\"one\" <= \"two\"", true],
+        ["\"one\" == \"two\"", false],
+        ["\"o\" > \"o\"", false],
+        ["\"o\" >= \"o\"", true],
+        ["\"o\" < \"o\"", false],
+        ["\"o\" <= \"o\"", true],
+        ["\"o\" == \"o\"", true],
 
         // new object [] { "5<-5>>>2", true },
-        ["5==5.0", true],
-        ["5!=5.0", false],
-        ["null in {true,false,null}", true],
-        ["null not in {true,false,null}", false],
-        ["null in {true,false,null}.ToArray()", true],
-        ["5 in {true,false,null}", false],
-        ["5 not in {true,false,null}", true],
+        ["5 == 5.0", true],
+        ["5 != 5.0", false],
+        ["null in {true, false, null}", true],
+        ["null not in {true, false, null}", false],
+        ["null in {true, false, null}.ToArray()", true],
+        ["5 in {true, false, null}", false],
+        ["5 not in {true, false, null}", true],
         ["5 instanceof System.Int32", true],
         ["5. instanceof System.Int32", false],
 
@@ -117,6 +127,11 @@ public class ArithmeticAndLogicalOperatorsTest : OgnlTestCase {
         ["4 ushr 2", 1],
         ["not null", true],
         ["not 1", false],
+        ["\"o\" gte \"o\"", true],
+        ["\"o\" gt \"o\"", false],
+        ["\"o\" lt \"o\"", false],
+        ["\"o\" lte \"o\"", true],
+        ["\"o\" eq \"o\"", true],
 
         ["#x > 0", true],
         ["#x < 0", false],
@@ -142,79 +157,16 @@ public class ArithmeticAndLogicalOperatorsTest : OgnlTestCase {
         ["\"1\" + #y", "11"]
     ];
 
-    public override TestSuite suite()
-    {
-        var result = new TestSuite();
-
-        for (var i = 0; i < Tests.Length; i++) {
-            result.addTest(new ArithmeticAndLogicalOperatorsTest((string)Tests[i][0] + " (" + Tests[i][1] + ")",
-                null, (string)Tests[i][0], Tests[i][1]));
-        }
-
-        return result;
-    }
-
-    public ArithmeticAndLogicalOperatorsTest()
-    {
-    }
-
-    public ArithmeticAndLogicalOperatorsTest(string name) : base(name)
-    {
-    }
-
-    public ArithmeticAndLogicalOperatorsTest(string name, object root, string expressionString,
-        object expectedResult, object setValue, object expectedAfterSetResult)
-        : base(name, root, expressionString, expectedResult, setValue, expectedAfterSetResult)
-    {
-    }
-
-    public ArithmeticAndLogicalOperatorsTest(string name, object root, string expressionString,
-        object expectedResult, object setValue)
-        : base(name, root, expressionString, expectedResult, setValue)
-    {
-    }
-
-    public ArithmeticAndLogicalOperatorsTest(string name, object root, string expressionString,
-        object expectedResult)
-        : base(name, root, expressionString, expectedResult)
-    {
-    }
-
     [SetUp]
-    public override void setUp()
+    public void setUp()
     {
-        base.setUp();
         context.Add("x", "1");
         context.Add("y", (decimal)1);
     }
 
-    [Test]
-    public void Test0()
+    [Test, TestCaseSource(nameof(Tests))]
+    public void CanPerformOperation(string expression, object expected)
     {
-        suite()[0].runTest();
-    }
-
-    [Test]
-    public void Test3()
-    {
-        suite()[3].runTest();
-    }
-
-    [Test]
-    public void Test13()
-    {
-        suite()[13].runTest();
-    }
-
-    [Test]
-    public void Test43()
-    {
-        suite()[43].runTest();
-    }
-
-    [Test]
-    public void Test47()
-    {
-        suite()[47].runTest();
+        Assert.That(Get(expression), Is.EqualTo(expected));
     }
 }
