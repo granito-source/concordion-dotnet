68ca0416291613f9d5cac7a17ee2c21a1c0a6a65
Alexei Yashkov <alexei.yashkov@yashkov.org>
2026-01-08 21:25:48 -0500

    Rework property tests

--- OGNL.Test/PropertyTest.cs
+++ OGNL.Test/PropertyTest.cs
@@ -1,8 +1,6 @@
-using OGNL.Test.Objects;
-using OGNL.Test.Util;
-
 //--------------------------------------------------------------------------
 //  Copyright (c) 2004, Drew Davidson and Luke Blanshard
+//  Copyright (c) 2026, Alexei Yashkov
 //  All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
@@ -31,79 +29,64 @@ using OGNL.Test.Util;
 //  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 //  DAMAGE.
 //--------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
 namespace OGNL.Test;
 
-public class PropertyTest : OgnlTestCase
-{
-    private static Root             ROOT = new();
+[TestFixture]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public class PropertyTest : OgnlFixture {
+    public static readonly string Size = "size";
 
-    private static object[][]       TESTS = [
-        [ROOT, "Map", ROOT.getMap()],
-        [ROOT, "Map.test", ROOT],
-        [ROOT, "Map[\"test\"]", ROOT],
-        [ROOT, "Map[\"te\" + \"st\"]", ROOT],
-        [ROOT, "Map[(\"s\" + \"i\") + \"ze\"]", ROOT.getMap() [Root.SIZE_STRING]],
-        [ROOT, "Map[\"size\"]", ROOT.getMap() [Root.SIZE_STRING]],
-        [ROOT, "Map[@Test.org.ognl.test.objects.Root@SIZE_STRING]", ROOT.getMap()[Root.SIZE_STRING]],
-        [ROOT.getMap(), "list", ROOT.getList()],
-        [ROOT, "Map.array[0]", ROOT.getArray()[0]],
-        [ROOT, "Map.list[1]", ROOT.getList() [1]],
-        [ROOT, "Map[^]", 99],
-        [ROOT, "Map[$]", null],
-        [ROOT.getMap(), "array[$]", ROOT.getArray()[ROOT.getArray().Length-1]],
-        [ROOT, "[\"Map\"]", ROOT.getMap()],
-        [ROOT.getArray(), "length", ROOT.getArray().Length],
-        [ROOT, "getMap().list[|]", ROOT.getList()[ROOT.getList().Count/2]],
-        [ROOT, "Map.(array[2] + size).ToString()", (ROOT.getArray()[2] + ROOT.getMap().Count).ToString ()],
-        [ROOT, "Map.(#this)", ROOT.getMap()],
-        [ROOT, "Map.(#this != null ? #this['size'] : null)", ROOT.getMap() [Root.SIZE_STRING]],
-        [ROOT, "Map[^].(#this == null ? 'empty' : #this)", 99],
-        [ROOT, "Map[$].(#this == null ? 'empty' : #this)", "empty"],
-        [ROOT, "Map[$].(#root == null ? 'empty' : #root)", ROOT]
+    private static readonly string[] TestArray = ["zero", "one", "two"];
+
+    private static readonly List<string> TestList = ["zero", "one", "two"];
+
+    private static readonly Dictionary<string, object> TestMap = new() {
+        ["test"] = 42,
+        ["size"] = "SIZE",
+        ["array"] = TestArray
+    };
+
+    private static readonly object?[][] Tests = [
+        ["Array", TestArray],
+        ["[\"Array\"]", TestArray],
+        ["Array[0]", "zero"],
+        ["Array[^]", "zero"],
+        ["Array[|]", "one"],
+        ["Array[$]", "two"],
+        ["List", TestList],
+        ["['List']", TestList],
+        ["List[0]", "zero"],
+        ["List[^]", "zero"],
+        ["List[|]", "one"],
+        ["List[$]", "two"],
+        ["Map", TestMap],
+        ["[\"Map\"]", TestMap],
+        ["Map.test", 42],
+        ["Map['test']", 42],
+        ["Map['te' + 'st']", 42],
+        ["Map['size']", "SIZE"],
+        ["Map[@OGNL.Test.PropertyTest@Size]", "SIZE"],
+        ["Map.size", 3],
+        ["Map.array[0]", "zero"],
+        ["Map.(array[1] + size)", "one3"],
+        ["Map.(#this)", TestMap],
+        ["Map.(#this != null ? #this['size'] : null)", "SIZE"],
+        ["Map[\"test\"].(#this == null ? 'empty' : #this)", 42],
+        ["Map[\"missing\"].(#this == null ? 'empty' : #this)", "empty"]
     ];
 
-    /*===================================================================
-        Public static methods
-      ===================================================================*/
-    public override TestSuite suite()
-    {
-        var       result = new TestSuite();
+    public string[] Array { get; } = TestArray;
 
-        for (var i = 0; i < TESTS.Length; i++) 
-        {
-            result.addTest(new PropertyTest((string)TESTS[i][1], TESTS[i][0], (string)TESTS[i][1], TESTS[i][2]));
-        }
-        return result;
-    }
+    public List<string> List { get; } = TestList;
 
-    /*===================================================================
-        Constructors
-      ===================================================================*/
-    public PropertyTest()
-    {
-	  
-    }
+    public Dictionary<string, object> Map { get; } = TestMap;
 
-    public PropertyTest(string name) : base(name)
+    [Test, TestCaseSource(nameof(Tests))]
+    public void GetsProperty(string expression, object? expected)
     {
-	    
-    }
-
-    public PropertyTest(string name, object root, string expressionString, object expectedResult, object setValue, object expectedAfterSetResult)
-        : base(name, root, expressionString, expectedResult, setValue, expectedAfterSetResult)
-    {
-        
-    }
-
-    public PropertyTest(string name, object root, string expressionString, object expectedResult, object setValue)
-        : base(name, root, expressionString, expectedResult, setValue)
-    {
-        
-    }
-
-    public PropertyTest(string name, object root, string expressionString, object expectedResult)
-        : base(name, root, expressionString, expectedResult)
-    {
-        
+        Assert.That(Get(expression), Is.EqualTo(expected));
     }
 }
