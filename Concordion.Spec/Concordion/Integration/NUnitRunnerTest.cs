a5adfabd8325bdd10ec6edc969f55312606698f6
Alexei Yashkov <alexei.yashkov@yashkov.org>
2026-01-17 11:29:33 -0500

    Remove Concordion.Runners -- not sure why one would need that

--- Concordion.Spec/Concordion/Integration/NUnitRunnerTest.cs
+++ Concordion.Spec/Concordion/Integration/NUnitRunnerTest.cs
@@ -1,9 +1,27 @@
-﻿using Concordion.Runners.NUnit;
+﻿using Concordion.Internal;
 using Concordion.Spec.Support;
+using NUnit.Framework;
 
 namespace Concordion.Spec.Concordion.Integration;
 
-public class NUnitRunnerTest : ExecutableSpecification {
+[TestFixture]
+public class NUnitRunnerTest {
+    [Test]
+    public void ConcordionTest()
+    {
+        var concordionResult = new FixtureRunner().Run(this);
+
+        if (concordionResult.HasExceptions)
+            throw new Exception(
+                "Exception in Concordion test: please see Concordion test reports");
+
+        if (concordionResult.HasFailures)
+            Assert.Fail($"""
+                Concordion Test Failures: {concordionResult.FailureCount}
+                for stack trace, please see Concordion test reports
+                """);
+    }
+
     public bool GreetingsProcessed(string fragment)
     {
         return new TestRig()
